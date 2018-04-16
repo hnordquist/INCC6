93b28fae8ceb15833225dae485b29f05fccb8789
Heather <heather@lanl.gov>
2018-04-16 10:28:15 -0600

    Changes to LM Acquire per Daniela. Enable QC and print results for Lm windows

---
+++ UnitTestINCC/TestDefs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestINCC
+{
+    [TestClass]
+    public class TestDefs
+    {
+        [TestMethod]
+        public void CreateCycleNonNull()
+        {
+            //arrange
+            //AnalysisDefs.Cycle cycle = new AnalysisDefs.Cycle ()
+            //assert
+        }
+    }
+}
